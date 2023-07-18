131861f1f52338ccc23ccd563d70856686438f0c
eqram1 <abmeqramhossain@gmail.com>
2023-07-18 07:54:58 +0600

    Style Header and footer and create simple banner

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,10 +3,10 @@ import logo from '../../../assets/logo.svg';
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-10 bg-black text-white">
             <div>
                 <img src={logo} alt="" />
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <p>Eqram Car Services Ltd.<br />Providing reliable tech since 1990</p>
             </div>
             <div>
                 <span className="footer-title">Services</span>
